69a50c4986d7cb0606fc8788dcf040ac6fc7ed0f
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-02-11 23:22:00 -0500

    filter_input and htmlspecialchars

--- display_discount.php
+++ display_discount.php
@@ -1,7 +1,7 @@
 <?php
-$Product_Description= $_POST['product_description'];
-$List_Price= $_POST['list_price'];
-$Discount_Percent= $_POST['discount_percent'];
+$Product_Description= filter_input(INPUT_POST, 'product_description');
+$List_Price= filter_input(INPUT_POST, 'list_price');
+$Discount_Percent= filter_input(INPUT_POST, 'discount_percent');
 
 $discount= $List_Price * $Discount_Percent * .01 ;
 $discount_price= $List_Price - $discount ;
@@ -22,13 +22,13 @@ $discount_price_formatted= "$".$discount_price;
         <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><b><?php echo $Product_Description; ?></b></span><br>
+        <span><?php echo htmlspecialchars($Product_Description); ?></span><br>
 
         <label>List Price:</label>
-        <span><i><?php echo $list_price_formatted; ?></i></span><br>
+        <span><?php echo htmlspecialchars($list_price_formatted); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><i><?php echo $list_percent_formatted; ?></i></span><br>
+        <span><?php echo $list_percent_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo $discount_formatted; ?></span><br>
