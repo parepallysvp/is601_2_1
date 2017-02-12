a38ac6a941e4a647ca89dd3842f534479ea611b0
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-02-11 19:43:32 -0500

    discount calculation

--- display_discount.php
+++ display_discount.php
@@ -3,8 +3,13 @@ $Product_Description= $_POST['product_description'];
 $List_Price= $_POST['list_price'];
 $Discount_Percent= $_POST['discount_percent'];
 
+$discount= $List_Price * $Discount_Percent * .01 ;
+$discount_price= $List_Price - $discount ;
+
 $list_price_formatted= "$".number_format($List_Price);
-$list_percent_formatted= $Discount_Percent."%"; 
+$list_percent_formatted= $Discount_Percent."%";
+$discount_formatted= "$".$discount;
+$discount_price_formatted= "$".$discount_price; 
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,10 +31,10 @@ $list_percent_formatted= $Discount_Percent."%";
         <span><?php echo $list_percent_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
