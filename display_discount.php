f609da6c030fb6403b1d7136acb43cf6dddeda7f
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-02-11 19:12:01 -0500

    post without formatting

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,9 @@
+<?php
+$Product_Description= $_POST['product_description'];
+$List_Price= $_POST['list_price'];
+$Discount_Percent= $_POST['discount_percent'];
+ 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,13 +15,13 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $Product_Description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $List_Price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $Discount_Percent; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
